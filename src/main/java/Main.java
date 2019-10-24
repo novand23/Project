d9136c51dd5b4e43e3cf762cc96ad16d9e656dcf
novand23 <novand23@gmail.com>
2019-10-24 20:31:46 +0300

    in

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,29 +6,41 @@ public class Main {
     private final static String USERNAME = "postgres";
     private final static String PASSWORD = "12345";
 
+
     public static void main(String[] args) {
         Connection connection;
 
         try {
             Driver driver = new org.postgresql.Driver();
             DriverManager.registerDriver(driver);
-
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             Statement statement = connection.createStatement();
-//            statement.execute("INSERT INTO usrs(id, name) VALUES (5, 'Igor2');");
-//            statement.execute("INSERT INTO usrs(id, name) VALUES (6, 'Igor3');");
+            /*statement.execute("INSERT INTO table1(id, name) VALUES (1, 'Andrew');");*/
 
-//            int i = statement.executeUpdate("UPDATE usrs SET name='Igor1' WHERE id = 1;");
+//            statement.addBatch("INSERT INTO table1(id, name) VALUES (3, 'Andrew');");
+//            statement.addBatch("INSERT INTO table1(id, name) VALUES (4, 'Andrew');");
+//            statement.addBatch("INSERT INTO table1(id, name) VALUES (5, 'Andrew');");
+//            statement.addBatch("INSERT INTO table1(id, name) VALUES (6, 'Andrew');");
+//            statement.executeBatch();
+//            statement.clearBatch();
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM usrs");
-            while (resultSet.next()){
+            statement.execute("DELETE FROM table1 WHERE id = 2");
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM table1");
+            while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + " - " + resultSet.getString("name"));
             }
+
+//            PreparedStatement ps = connection.prepareStatement("INSERT INTO table1(id, name) VALUES (?, ?);");
+//            ps.setInt(1, 2);
+//            ps.setString(2, "Alex");
+//            ps.execute();
 
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
+
+
 }
